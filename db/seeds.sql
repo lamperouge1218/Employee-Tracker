cfd6bb894795df3cf3bedc62ea4f64b235c80b0c
lamperouge <philbohn1791@gmail.com>
2021-10-09 13:06:25 -0400

    added manager roles to the _role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,11 +10,17 @@ VALUES
 INSERT INTO _role(title, salary, deparment_id)
 VALUES
     ("Tech Support Specialist", 36000, 1),
+    ("Senior Tech Support", 65000, 1),
     ("Account Coordinator", 36000, 2),
+    ("Senior Account Coordinator", 65000, 2),
     ("Asset Recovery Specialist", 40000, 3),
+    ("Senior Asset Recovery", 70000, 3),
     ("Engineer Team Member", 70000, 4),
+    ("Engineer Team Lead", 100000, 4),
     ("IT Specialist", 70000, 5),
-    ("Shipping Team Member", 35000, 6);
+    ("IT Team Lead", 100000, 5),
+    ("Shipping Team Member", 35000, 6),
+    ("Shipping Team Lead", 65000, 6);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
